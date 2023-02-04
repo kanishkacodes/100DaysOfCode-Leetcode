2c92389c6f5e5ba389e71d88e7c334ebf3ade46b
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-02-04 17:12:41 +0530

    Time: 138 ms (48.81%), Space: 71.6 MB (6.87%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+           ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         unordered_map<int,int>m;
         for (int i = 0 ; i < nums.size() ; ++i){
             m[nums[i]]++;
@@ -10,6 +12,9 @@ public:
                 return true;
             }
             }
+        
+     
+        
         
         return false;
     }
