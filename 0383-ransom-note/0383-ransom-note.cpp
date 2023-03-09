49b9e4df48aebd514a8967825e8db2439b17d207
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-03-09 22:15:06 +0530

    Time: 11 ms (87.44%), Space: 8.8 MB (48.04%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+       
+        int counter[26] = {0};
+        
+        for(char ch : magazine)
+            counter[ch - 'a']++;
+        
+        for(char ch : ransomNote)
+           
+            if(counter[ch - 'a']-- <= 0)
+                return false;
+        return true;      
+    }
+};
