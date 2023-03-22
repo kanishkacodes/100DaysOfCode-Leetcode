92272b35c74ec513789df046a4303173733fc59d
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-03-22 23:06:51 +0530

    Time: 745 ms (16.31%), Space: 192.5 MB (6.01%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        
-        //Starting with the initial node 1, add each connected node who hasn't been visited yet to the queue.
-        //Since we only add connected nodes, any node within our queue will have the same minimum disdtance of a 
-        //road in its path as any other node who has been added to the queue. Knowing this, keep track of a single
-        //minimum value which we will return once we iterate through all of the elements in the queue since
-        //that will let us know that we have checked all roads between all elements which are connected to 1 and n
+
 
         std::unordered_set<int> visitSet;
-        //Make an adjacency list with each node and who it's connected to and the distances from that node.
-        std::unordered_map<int, std::unordered_map<int, int>> srcToDestToDist;
+   std::unordered_map<int, std::unordered_map<int, int>> srcToDestToDist;
         int returnVal = INT_MAX;
 
         for(const auto& road : roads)
