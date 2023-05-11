63862e197e3cfc129486e534e351a109fc5abe9f
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-05-11 21:44:38 +0530

    Time: 33 ms (56.59%), Space: 24.9 MB (93.87%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -9,7 +9,7 @@ class Solution
            	//For example, let's say we have an array nums with size n = 5 and we want to rotate it by k = 7 positions. Without the adjustment, rotating by k = 7 would result in unnecessary full rotations. However, with the adjustment k = k % n, we have k = 7 % 5 = 2, which limits the rotation to the desired amount.
             
             
-            // Main Logic : Reverse the whole array---Reverse the first k elements---Reverse the remaining n-k elements
+            // Main Logic : Reverse the whole array---Reverse the first k elements---Reverse the remaining n-k elements--then we will get the rotated array.
             reverse(nums.begin(), nums.end());	// Reverse the whole array
             reverse(nums.begin(), nums.begin() + k);	// Reverse the first k elements
             reverse(nums.begin() + k, nums.end());	// Reverse the remaining n-k elements
