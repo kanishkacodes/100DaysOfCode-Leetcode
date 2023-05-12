13a23ddb3d43029af3e10af9e9fda2a1125c3178
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-05-12 14:14:19 +0530

    Time: 14 ms (26.95%), Space: 13 MB (90.30%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,9 +4,12 @@ class Solution
         int maxProfit(vector<int> &prices)
         {
             int maxProfit = 0;
+            
+            // Logic : Calculte the profit and add them.
+            
             for (int i = 1; i < prices.size(); i++)
             {
-                if (prices[i] > prices[i - 1])
+                if (prices[i] > prices[i - 1]) // Profit Should not be negative
                 {
                     int best = prices[i] - prices[i - 1];
                     maxProfit+=best;
