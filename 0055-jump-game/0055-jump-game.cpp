83ca5ee3758ed842ce808311f5f6d936874460df
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-05-15 11:17:16 +0530

    Time: 79 ms (33.53%), Space: 48.3 MB (77.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,8 +3,8 @@ class Solution
     public:
         bool canJump(vector<int> &nums)
         {
-            int n = nums.size() - 1;
-            for (int i = nums.size() - 1; i >= 0; i--)
+            int n = nums.size()-1;
+            for (int i = nums.size()-1; i >= 0; i--)
             {
                 if (i + nums[i] >= n)
                 {
