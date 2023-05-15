aa06022b108e6f355aa4218c89531e23b0d0beff
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-05-15 11:23:25 +0530

    Time: 74 ms (46.19%), Space: 48.4 MB (77.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,10 +3,10 @@ class Solution
     public:
         bool canJump(vector<int> &nums)
         {
-            int n = nums.size()-1;
-            for (int i = nums.size()-1; i >= 0; i--)
+            int n = nums.size() - 1;
+            for (int i = nums.size() - 1; i >= 0; i--)
             {
-                if (i + nums[i] >= n)
+                if (i + nums[i] >= n)	//The condition i + nums[i] >= n is checking whether the current index i combined with the maximum jump value from that position, nums[i], is greater than or equal to the previous minimum index n.
                 {
                     n = i;
                 }
