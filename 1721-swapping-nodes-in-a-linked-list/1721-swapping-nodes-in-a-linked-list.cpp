d067d6a568df6c128a4e6f12a98bb30cca99a684
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-05-18 14:19:33 +0530

    Time: 648 ms (99.61%), Space: 180.1 MB (30.63%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* leftNode = findKthNode(head, k);
-        ListNode* rightNode = findKthNodeFromEnd(head, k);
-
-        swap(leftNode->val, rightNode->val);
-
+        ListNode *left_ptr = head, *right_ptr = head;
+        int i = 0;
+        while (i < k-1) {
+            right_ptr = right_ptr->next;
+            i++;
+        }
+        
+        ListNode *end_ptr = right_ptr;
+        while (right_ptr->next) {
+            left_ptr = left_ptr->next;
+            right_ptr = right_ptr->next;
+        }
+        
+        swap(end_ptr->val, left_ptr->val);
+        
         return head;
     }
-
-private:
-    ListNode* findKthNode(ListNode* head, int k) {
-        ListNode* currentNode = head;
-        for (int i = 1; i < k; i++) {
-            currentNode = currentNode->next;
-        }
-        return currentNode;
-    }
-
-    ListNode* findKthNodeFromEnd(ListNode* head, int k) {
-        ListNode* slowPtr = head;
-        ListNode* fastPtr = head;
-
-        for (int i = 0; i < k; i++) {
-            fastPtr = fastPtr->next;
-        }
-
-        while (fastPtr != nullptr) {
-            slowPtr = slowPtr->next;
-            fastPtr = fastPtr->next;
-        }
-
-        return slowPtr;
-    }
 };
+
