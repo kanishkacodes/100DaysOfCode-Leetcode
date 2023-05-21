67363d6116f5cddc9e036b4964055c03f4a51fe8
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-05-21 12:32:55 +0530

    Time: 32 ms (66.00%), Space: 24.8 MB (92.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,11 @@
-class Solution
-{
-    public:
-        void rotate(vector<int> &nums, int k)
-        {
-            int n = nums.size();
-            k = k % n;	// Adjust k if it is greater than n
-
-           	//For example, let's say we have an array nums with size n = 5 and we want to rotate it by k = 7 positions. Without the adjustment, rotating by k = 7 would result in unnecessary full rotations. However, with the adjustment k = k % n, we have k = 7 % 5 = 2, which limits the rotation to the desired amount.
-            
-            
-            // Main Logic : Reverse the whole array---Reverse the first k elements---Reverse the remaining n-k elements--then we will get the rotated array.
-            reverse(nums.begin(), nums.end());	// Reverse the whole array
-            reverse(nums.begin(), nums.begin() + k);	// Reverse the first k elements
-            reverse(nums.begin() + k, nums.end());	// Reverse the remaining n-k elements
-        }
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
 };
