6665c28c7565816cc836418358e8916f5f5e5e79
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-06-01 10:39:45 +0530

    Time: 16 ms (70.70%), Space: 11.4 MB (92.39%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -8,44 +8,49 @@
  *    ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
-class Solution
-{
-    public:
-        ListNode* reverseKGroup(ListNode *head, int k)
-        {
-
-            if (head == NULL || k == 1)
-            {
-                return head;
-            }
-
-            ListNode *dummy = new ListNode(0);
-            dummy->next = head;
-
-            ListNode *cur = dummy, *nex = dummy, *pre = dummy;
-            int count = 0;
-
-            while (cur->next != NULL)
-            {
-                cur = cur->next;
-                count++;
-            }
-
-            while (count >= k)
-            {
-                cur = pre->next;
-                nex = cur->next;
-                for (int i = 0; i < k - 1; ++i)
-                {
-                    cur->next = nex->next;
-                    nex->next = pre->next;
-                    pre->next = nex;
-                    nex = cur->next;
-                }
-                pre = cur;
-                count -= k;
-            }
-
-            return dummy->next;
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        // Segment 1: Check if head is NULL or k is 1
+        if (head == NULL || k == 1) {
+            return head;
         }
+        
+        // Segment 2: Create a dummy node and set its next pointer to the head
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        
+        // Segment 3: Initialize three pointers: cur, nex, and pre to the dummy node
+        ListNode* cur = dummy; // Pointer to the current node in the group
+        ListNode* nex = dummy; // Pointer to the next node in the group
+        ListNode* pre = dummy; // Pointer to the previous node before the group
+        int count = 0; // Counter to keep track of the number of nodes
+        
+        // Segment 4: Count the number of nodes in the list
+        while (cur->next != NULL) {
+            cur = cur->next;
+            count++;
+        }
+        
+        // Segment 5: Reverse the list in groups of k
+        while (count >= k) {
+            cur = pre->next; // Move cur pointer to the start of the current group
+            nex = cur->next; // Move nex pointer to the next node after the current group
+            
+            // Segment 6: Reverse a group of k nodes
+            for (int i = 0; i < k - 1; ++i) {
+                cur->next = nex->next; // Reverse the link between current node and next node
+                nex->next = pre->next; // Reverse the link between next node and previous group
+                pre->next = nex; // Update the next pointer of the previous group to the current node
+                nex = cur->next; // Move nex pointer to the next node for the next iteration
+            }
+            
+            // Segment 7: Move pre to the last node of the reversed group
+            pre = cur;
+            count -= k;
+        }
+        
+        // Segment 8: Return the next pointer of the dummy node, which is the head of the reversed list
+        return dummy->next;
+    }
 };
