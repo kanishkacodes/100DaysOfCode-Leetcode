54f516558eab2cc284d48b2ae6484174eced87f2
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-06-01 14:59:58 +0530

    Time: 8 ms (70.89%), Space: 11.3 MB (35.68%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,48 +17,57 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        // Initialize two pointers, 'iter' and 'front'
         Node *iter = head; 
-          Node *front = head;
+        Node *front = head;
 
-          // First round: make copy of each node,
-          // and link them together side-by-side in a single list.
-          while (iter != NULL) {
+        // First round: make a copy of each node and link them together side-by-side in a single list.
+        while (iter != NULL) {
             front = iter->next;
 
+            // Create a new node with the same value as the current node
             Node *copy = new Node(iter->val);
+
+            // Connect the new node to the original node and the next node
             iter->next = copy;
             copy->next = front;
 
+            // Move to the next pair of nodes
             iter = front;
-          }
+        }
 
-          // Second round: assign random pointers for the copy nodes.
-          iter = head;
-          while (iter != NULL) {
+        // Second round: assign random pointers for the copy nodes.
+        iter = head;
+        while (iter != NULL) {
             if (iter->random != NULL) {
-              iter->next->random = iter->random->next;
+                // Set the random pointer of the copied node to the corresponding copied random node
+                iter->next->random = iter->random->next;
             }
+            
+            // Move to the next original node
             iter = iter->next->next;
-          }
+        }
 
-          // Third round: restore the original list, and extract the copy list.
-          iter = head;
-          Node *pseudoHead = new Node(0);
-          Node *copy = pseudoHead;
+        // Third round: restore the original list and extract the copy list.
+        iter = head;
+        Node *pseudoHead = new Node(0);
+        Node *copy = pseudoHead;
 
-          while (iter != NULL) {
+        while (iter != NULL) {
             front = iter->next->next;
 
-            // extract the copy
+            // Extract the copied node and connect it to the copy list
             copy->next = iter->next;
 
-            // restore the original list
+            // Restore the original list by connecting the original nodes
             iter->next = front;
-              
-            copy = copy -> next; 
-            iter = front;
-          }
 
-          return pseudoHead->next;
+            // Move to the next pair of nodes
+            copy = copy->next; 
+            iter = front;
+        }
+
+        // Return the copy list (excluding the pseudoHead)
+        return pseudoHead->next;
     }
 };
