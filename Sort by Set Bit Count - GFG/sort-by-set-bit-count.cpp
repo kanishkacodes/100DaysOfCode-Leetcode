9a6c180492688f60f3f5540d93fca671210f4a2d
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-06-06 17:00:22 +0530

    Added solution - LeetHub

--- Sort by Set Bit Count - GFG/sort-by-set-bit-count.cpp
+++ Sort by Set Bit Count - GFG/sort-by-set-bit-count.cpp
@@ -5,16 +5,22 @@ using namespace std;
 
 // } Driver Code Ends
 
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
+    // Comparison function to compare integers based on set bit count
     static bool comp(int a, int b) {
         return __builtin_popcount(a) > __builtin_popcount(b);
     }
     
+    // Function to sort the array based on set bit count of elements
     void sortBySetBitCount(int arr[], int n) {
         stable_sort(arr, arr + n, comp);
     }
 };
+
 
 //{ Driver Code Starts.
 
