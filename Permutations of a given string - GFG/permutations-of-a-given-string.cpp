8c9f13f11519e347d690c39d3544c4b23a010823
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-06-17 20:29:04 +0530

    Added solution - LeetHub

---
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    vector<string> find_permutation(string S) {
+        vector<string> permutations;
+        sort(S.begin(), S.end());  // Sort the string to get permutations in lexicographically sorted order
+        vector<bool> used(S.length(), false);  // Keep track of used characters
+        string currentPermutation;
+        findUniquePermutations(S, used, currentPermutation, permutations);
+        return permutations;
+    }
+
+private:
+    void findUniquePermutations(const string& S, vector<bool>& used, string& currentPermutation, vector<string>& permutations) {
+        if (currentPermutation.length() == S.length()) {
+            permutations.push_back(currentPermutation);
+            return;
+        }
+
+        for (int i = 0; i < S.length(); i++) {
+            if (used[i] || (i > 0 && S[i] == S[i - 1] && !used[i - 1]))
+                continue;  // Skip used or duplicate characters
+
+            used[i] = true;
+            currentPermutation.push_back(S[i]);
+            findUniquePermutations(S, used, currentPermutation, permutations);
+            currentPermutation.pop_back();
+            used[i] = false;
+        }
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
