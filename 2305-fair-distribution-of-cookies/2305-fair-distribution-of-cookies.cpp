ee33e726e7f45353241f7e4d2c41bb756b956cde
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-07-01 20:35:59 +0530

    Time: 1506 ms (56.35%), Space: 6.8 MB (98.89%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,28 @@
+#include <climits>
+#include <vector>
+
+class Solution {
+public:
+    int ans = INT_MAX;
+
+    void helper(const std::vector<int>& cookies, int start, int k, std::vector<int>& temp) {
+        if (start == cookies.size()) {
+            int max = 0;
+            for (int c : temp)
+                max = std::max(max, c);
+            ans = std::min(ans, max);
+            return;
+        }
+        for (int i = 0; i < k; i++) {
+            temp[i] += cookies[start];
+            helper(cookies, start + 1, k, temp);
+            temp[i] -= cookies[start];
+        }
+    }
+
+    int distributeCookies(const std::vector<int>& cookies, int k) {
+        std::vector<int> temp(k, 0);
+        helper(cookies, 0, k, temp);
+        return ans;
+    }
+};
