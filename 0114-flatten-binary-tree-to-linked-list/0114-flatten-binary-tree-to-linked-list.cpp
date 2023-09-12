752a79ef04ec01714623508349fcd97793217401
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-09-12 22:25:10 +0530

    Time: 5 ms (56.06%), Space: 12.8 MB (35.14%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -12,25 +12,35 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        if( root )
-        {
-            /*Move the left node to the right node*/
-            TreeNode* temp = root->right;
-            root->right = root->left;
-            root->left = nullptr;
-            TreeNode* node = root;
-            
-            /*Move to the end of the prev left node which is the new right node*/
-            while( node->right )
-            {
-                node = node->right;
-            }
-            
-            /*Append the right node to its end*/
-            node->right = temp;
-            flatten( root->right ); 
-        } 
-        return;
-               
+        if (root) {
+            flattenHelper(root);
+        }
+    }
+
+private:
+    TreeNode* flattenHelper(TreeNode* node) {
+        if (!node->left && !node->right) {
+            return node;
+        }
+
+        TreeNode* leftTail = nullptr;
+        TreeNode* rightTail = nullptr;
+
+        if (node->left) {
+            leftTail = flattenHelper(node->left);
+        }
+
+        if (node->right) {
+            rightTail = flattenHelper(node->right);
+        }
+
+        if (leftTail) {
+            leftTail->right = node->right;
+            node->right = node->left;
+            node->left = nullptr;
+        }
+
+        return rightTail ? rightTail : leftTail;
     }
 };
+
