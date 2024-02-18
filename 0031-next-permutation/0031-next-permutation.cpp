6b6569811cc85a21bb72a46549a473bc8824cd66
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-02-18 17:01:35 +0530

    Time: 0 ms (100.00%), Space: 14.5 MB (44.61%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -6,6 +6,9 @@ public:
         int n = nums.size();
         
         
+        //First find the dip (like what is smaller after a big number) and store the dip in index 
+        
+        
         for (int i = n-2 ; i >= 0 ; i--){
             
             if (nums[i]<nums[i+1]){
@@ -17,15 +20,16 @@ public:
             
         }
         
+        //if there is no dip
         if ( index == -1 )  {
                 
                 reverse(nums.begin(), nums.end());
             }
         
-        
+        // or if it has a dip
         else{
          
-        
+        //swap the dip with the next smallest number
         for (int i = n-1 ; i > index ; i--){
             
             if (nums[i] > nums[index]){
@@ -35,7 +39,7 @@ public:
             }
         }
         
-        
+        // at the end reverse the rmaining array it will be sorted
         reverse(nums.begin()+index+1, nums.end());
             
         }
