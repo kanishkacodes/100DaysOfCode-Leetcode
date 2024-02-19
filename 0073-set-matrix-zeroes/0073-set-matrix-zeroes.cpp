8e4cb32c6e1a9e768ef479f3869002a89a17d14b
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-02-19 13:32:01 +0530

    Time: 7 ms (88.48%), Space: 16.1 MB (37.25%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,31 +1,63 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> visited = matrix;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    for(int k=0; k<m; k++){
-                        visited[i][k] = 0;
+        
+        // Find the size of rows (m)
+    int m = matrix.size();
+
+    // Find the size of columns (n)
+    int n = 0;
+    if (m > 0) {
+        n = matrix[0].size();
+    }
+
+        int col0 = 1;
+        
+        for (int i = 0 ; i < m ; i++){
+            
+            for (int j = 0 ; j < n ; j++){
+                
+                if (matrix[i][j] == 0){
+                    
+                    matrix[i][0] = 0;
+                    
+                    if (j != 0){
+                        
+                        matrix[0][j] = 0;
+                    }
+                    else{
+                        col0 = 0;
                     }
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    for(int k=0; k<n; k++){
-                        visited[k][j] = 0;
+        
+        
+        for (int i = 1 ; i < m ; i++){
+            for (int j = 1 ; j < n ; j++){
+                if (matrix[i][j] != 0){
+                    
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0){
+                        
+                        matrix[i][j] = 0;
                     }
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                matrix[i][j] = visited[i][j];
+        
+        if (matrix[0][0] == 0 ){
+            
+            for (int j = 0 ; j < n ; j++){
+                matrix[0][j] = 0;
             }
         }
+        
+        if (col0 == 0){
+            
+            for (int i = 0 ; i < m ; i++){
+                matrix[i][0] = 0 ; 
+            }
+        }
+        
     }
 };
