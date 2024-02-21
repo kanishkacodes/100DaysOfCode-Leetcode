cccee6006086e694e99df8ee3790cba15100d517
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-02-22 00:03:01 +0530

    Time: 3 ms (99.36%), Space: 23.2 MB (14.50%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,39 +1,37 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    int trap(vector<int>& v) {
-        int n = v.size(),i;
-        vector<int> l(n,-1),r(n,-1),s;
-        for(i = 0; i < n; i++){
-            while(!s.empty()&&s.back()<=v[i]){
-                s.pop_back();
-            }
-            if(!s.empty()){
-                l[i] = s.back();
-                if(v[i]>s.back())
-                s.push_back(v[i]);
-            }else{
-                s.push_back(v[i]);
-            }
+    int trap(std::vector<int>& height) {
+        int n = height.size();
+
+        if (n <= 2) {
+            return 0; // No trapping is possible with less than 3 elements
         }
-        s.clear();
-        for(i = n-1; i >= 0; i--){
-            while(!s.empty()&&s.back()<=v[i]){
-                s.pop_back();
-            }
-            if(!s.empty()){
-                r[i] = s.back();
-                if(v[i]>s.back())
-                s.push_back(v[i]);
-            }else{
-                s.push_back(v[i]);
-            }
+
+        std::vector<int> lb(n);
+        std::vector<int> rb(n);
+
+        // Filling the left boundary
+        lb[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            lb[i] = std::max(lb[i - 1], height[i]);
         }
-        int ans = 0;
-        for(i = 0; i < n; i++){
-            if(l[i] != -1 && r[i] != -1){
-                ans += min(l[i],r[i])-v[i];
-            }
+
+        // Filling the right boundary
+        rb[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rb[i] = std::max(rb[i + 1], height[i]);
         }
-        return ans;
+
+        int totalWater = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            int possibleWaterHeight = std::min(lb[i], rb[i]);
+            totalWater += std::max(0, possibleWaterHeight - height[i]);
+        }
+
+        return totalWater;
     }
 };
