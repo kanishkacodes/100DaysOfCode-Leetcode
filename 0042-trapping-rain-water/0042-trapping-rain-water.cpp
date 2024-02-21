cccd53682d5c4a72678fc10364c6a2acc4b7f1c9
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-02-22 00:03:45 +0530

    Time: 8 ms (78.51%), Space: 23.2 MB (8.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,6 +1,3 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
     int trap(std::vector<int>& height) {
@@ -10,26 +7,26 @@ public:
             return 0; // No trapping is possible with less than 3 elements
         }
 
-        std::vector<int> lb(n);
-        std::vector<int> rb(n);
+        vector<int> lb(n);
+        vector<int> rb(n);
 
         // Filling the left boundary
         lb[0] = height[0];
         for (int i = 1; i < n; i++) {
-            lb[i] = std::max(lb[i - 1], height[i]);
+            lb[i] = max(lb[i - 1], height[i]);
         }
 
         // Filling the right boundary
         rb[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            rb[i] = std::max(rb[i + 1], height[i]);
+            rb[i] = max(rb[i + 1], height[i]);
         }
 
         int totalWater = 0;
 
         for (int i = 1; i < n - 1; i++) {
             int possibleWaterHeight = std::min(lb[i], rb[i]);
-            totalWater += std::max(0, possibleWaterHeight - height[i]);
+            totalWater += max(0, possibleWaterHeight - height[i]);
         }
 
         return totalWater;
