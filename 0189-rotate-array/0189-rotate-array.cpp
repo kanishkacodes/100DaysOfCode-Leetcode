f1c6c1af6a08820e29d39cea2c668a15b5289516
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-02-22 13:43:56 +0530

    Time: 18 ms (85.09%), Space: 27.3 MB (44.50%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,30 @@
+#include <vector>
+
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
+    
+    void reverseArray(std::vector<int>& nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    void rotate(std::vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;
         
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
+        k = k % n; // To handle cases where k > n
+
+        // Reverse the entire array
+        reverseArray(nums, 0, n - 1);
+        
+        // Reverse the first part of the array (0 to k-1)
+        reverseArray(nums, 0, k - 1);
+        
+        // Reverse the second part of the array (k to n-1)
+        reverseArray(nums, k, n - 1);
     }
 };
