8c4f764ccc5615edee4bc5a3ad27efcff9d83a91
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-02-26 23:58:32 +0530

    Time: 84 ms (71.61%), Space: 87.7 MB (66.37%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -7,8 +7,12 @@ public:
         if (size != m * n) {
             return {};
         }
+        
+        //initializes a 2D vector named result with dimensions m rows and n columns. Each element in the vector is    initialized with the value 0.
 
         vector<vector<int>> result(m, vector<int>(n, 0));
+        
+        
 
         // Fill the 2D array by iterating through the original vector
         for (int i = 0; i < size; ++i) {
@@ -18,3 +22,19 @@ public:
         return result;
     }
 };
+
+// When i is 0:
+
+// mat[0 / 2][0 % 2] evaluates to mat[0][0]
+// When i is 1:
+
+// mat[1 / 2][1 % 2] evaluates to mat[0][1]
+// When i is 2:
+
+// mat[2 / 2][2 % 2] evaluates to mat[1][0]
+// When i is 3:
+
+// mat[3 / 2][3 % 2] evaluates to mat[1][1]
+// When i is 4:
+
+// mat[4 / 2][4 % 2] evaluates to mat[2][0]
