6d7774a836c3e8f5c88b4bde829d6c57ecaa32dc
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-03-11 10:43:38 +0530

    Time: 3 ms (89.69%), Space: 16.1 MB (42.56%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,49 @@
 class Solution {
 public:
-    int leftMost(vector<int>& nums, int target) {
-        int end = nums.size() - 1;
-        int start = 0;
-        int mid;
-        int ans = -1;  // Initialize ans
-
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                end = mid - 1;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
-    int rightMost(vector<int>& nums, int target) {
-        int end = nums.size() - 1;
-        int start = 0;
-        int mid;
-        int ans = -1;  // Initialize ans
-
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                start = mid + 1;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int left = leftMost(nums, target);
-        int right = rightMost(nums, target);
-
-        return {left, right};
+        
+        vector<int> ans = {-1,-1};
+        
+        //find the left most index of the target
+        int low = 0;
+        int high = nums.size()-1;
+        while(low <= high){
+            
+            int mid = (high+low)/2;
+            
+            if (nums[mid]==target){
+                ans[0] = mid;
+                high = mid-1;         // continue searching on the left side
+            }
+            else if (nums[mid] < target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+            
+        }
+        
+        //find the right most index of the target
+        low = 0;
+        high = nums.size()-1;
+        while(low <= high){
+            
+            int mid = (low+high)/2;
+            
+            if (nums[mid] == target){
+                ans[1] = mid;
+                low = mid+1;
+            }
+            else if(nums[mid] > target){
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+        
+        return ans;
+        
     }
 };
