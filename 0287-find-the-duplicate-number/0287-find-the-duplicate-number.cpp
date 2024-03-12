d489ab0c20b963d0658add8fcb30307e89a05b53
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-03-12 15:35:34 +0530

    Time: 95 ms (44.54%), Space: 63.6 MB (70.07%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findDuplicate(std::vector<int>& nums) {
+        int low = 1;
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+
+            if (count <= mid) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+};
+
+//The inner for loop has a time complexity of O(n) because it iterates over the entire array of length n. However, since it is nested inside the while loop, and the while loop performs a binary search with log n iterations, the overall time complexity becomes O(n log n).
+
