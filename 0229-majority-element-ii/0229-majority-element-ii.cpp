f78c53e89a07134bacd924ff7635b4caa3a7882c
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-04-10 16:56:13 +0530

    Time: 8 ms (66.63%), Space: 19.5 MB (19.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-     int n=nums.size();
-       vector<int>v;
-       unordered_map<int,int>m;
-        for(int i=0; i<n; i++){
-         
-         m[nums[i]]++;
-         
-         if(m[nums[i]]>n/3){
-            
-             v.push_back(nums[i]);
-             
-             m[nums[i]]=-1;  
-         }
-     }  
-     
-        return v;
+        unordered_map<int, int> m;
+        
+        // Count occurrences of each number
+        for(int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        
+        vector<int> result;
+        int threshold = nums.size() / 3;
+        
+        // Check if the count of each number exceeds the threshold
+        for (auto it : m) {
+            if (it.second > threshold) {
+                result.push_back(it.first);
+            }
+        }
+        
+        return result;
     }
 };
