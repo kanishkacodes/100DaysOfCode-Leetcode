d0d7c9dad957b2570a91a456f77f0bdac5c20dad
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-04-18 13:05:20 +0530

    Time: 58 ms (85.37%), Space: 45 MB (57.87%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefixSumFreq;
+        
+        int n = nums.size();
         int count = 0;
-        int sum = 0;
+        int presum = 0;
+        unordered_map<int,int>m;
         
-        // Add 0 as prefix sum with frequency 1 to handle subarrays starting from index 0
-        prefixSumFreq[0] = 1;
+         m[0] = 1;
         
-        for (int i = 0; i < nums.size(); ++i) {
+        
+        for (int i = 0 ; i < n ; i++){
             
-            sum += nums[i];
-            // Check if there exists a prefix sum (sum - k)
-            // If yes, then there exists a subarray with sum k
-            if (prefixSumFreq.find(sum - k) != prefixSumFreq.end()) {
-                count += prefixSumFreq[sum - k];
+            presum+=nums[i];
+            
+            if (m.find(presum-k) != m.end()){  // if the presum-k is present
+                
+                count+= m[presum-k];  // increse the count by presum-k value in map
+                
             }
-            // Increment the frequency of the current prefix sum
-            prefixSumFreq[sum]++;
+            
+            m[presum]++;   // added new presum or incresed its frequency/ count
         }
         
+        
         return count;
+        
     }
 };
