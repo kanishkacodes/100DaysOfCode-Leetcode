69ec0abc15bbcac4c85be09c99afb194803e6028
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-04-25 23:17:07 +0530

    Time: 0 ms (100.00%), Space: 13 MB (99.73%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,8 +8,15 @@ public:
         for (int i = 0; i < n; i++) 
             fast = fast->next;
         
-        if (fast == NULL) 
+        
+// Move both fast and slow pointers simultaneously until the fast pointer reaches the last node.
+// At this point, the slow pointer will be pointing to the node just before the one to be removed.  
+        
+        
+        if (fast == NULL)  
             return head->next;
+        
+        
         
         while (fast->next != NULL) {
             fast = fast->next; 
