40e32228f44dbe3bc14ae90de64dadd5bcddd6af
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-04-26 22:21:30 +0530

    Time: 20 ms (97.47%), Space: 17 MB (78.75%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& arr) {
+        
+        int n = arr.size() ;
+        vector<int> dp ;
+        dp = arr[n-1] ;
+        
+        for(int i=n-2;i>=0;i--){
+            int firstmin=INT_MAX,secondmin=INT_MAX,firstminindex ;
+            for (int j=0;j<n;j++){
+                if (firstmin>dp[j]){                    //calculating first min
+                    firstmin = dp[j] ;
+                    firstminindex = j ;
+                }
+            }for (int j=0;j<n;j++){
+                if (j==firstminindex)continue ;                          //second min
+                secondmin = min(secondmin,dp[j]) ;
+            }
+            for (int j=0;j<n;j++){
+                if (j==firstminindex) dp[j]=arr[i][j]+secondmin ;  
+                else dp[j] = arr[i][j]+firstmin ;
+            }
+        }
+        return *min_element(dp.begin(),dp.end());
+    }
+};
