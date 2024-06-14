b64a1d43bd30541cd49af416bac7b1f6a359b663
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2024-06-14 17:19:42 +0530

    Time: 41 ms (46.43%), Space: 17.1 MB (69.15%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,55 +1,65 @@
 class Solution {
 public:
     
-    // find the length
-    int lengthofll(ListNode *head){
+    int length(ListNode * head){
+        
         int len = 0;
         ListNode *curr = head;
-        while (curr != NULL){
+        while (curr !=NULL){
+            
             len++;
             curr = curr->next;
         }
+        
         return len;
+        
     }
     
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
-        // decleare two pointers 
-        ListNode *curr1 = headA;
-        ListNode *curr2 = headB;
+        ListNode* curr1 = headA;
+        ListNode* curr2 = headB;
         
-        int lengthA = lengthofll(headA);
-        int lengthB = lengthofll(headB);
+        int len1 = length(headA);
+        int len2 = length(headB);
         int diff;
         
-        //put the pointers in parallel 
-
-        if (lengthA > lengthB){
-            diff =  lengthA - lengthB;
+        if (len1>len2){
+            
+            diff = len1-len2;
+            
             while (diff != 0){
                 curr1 = curr1->next;
                 diff--;
             }
         }
-        else{
-            diff = lengthB - lengthA;
+        
+        else {
+            
+            diff = len2-len1;
+            
             while (diff != 0){
                 curr2 = curr2->next;
                 diff--;
             }
         }
         
-        
-        // check the intersection point
-        
-        while (curr1 != NULL && curr2 != NULL){
-            if (curr1 == curr2){ 
+        while ( curr1 != NULL && curr2 != NULL){
+            
+             if (curr1 == curr2){
                 return curr1;
             }
+            
             curr1 = curr1->next;
             curr2 = curr2->next;
+            
+           
+            
         }
         
         return NULL;
+        
+        
+        
     }
 };
